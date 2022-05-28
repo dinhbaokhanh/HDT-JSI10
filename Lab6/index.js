eb75defa3f8d9831d0ae6d9571547987f377f21f
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2022-05-28 13:59:58 +0700

    commit

---
+++ Lab6/index.js
@@ -0,0 +1,43 @@
+//B1
+// var cart = [
+//     ["Grape",15],
+//     ["Apple",10],
+//     ["Mango",20],
+// ]
+//console.log('I have ' + cart[0][1] + ' ' + cart[0][0]+'s')
+
+
+
+//B2
+// for(let i = 0; i < cart.length; i ++) {
+//     let len = cart[i].length
+//     for (let j = 0; j < len; j ++){
+//         console.log('I have ' + cart[i][j+1] + ' ' + cart[i][j]+'s')
+//     }
+// } //:>>
+
+// for(let x in cart) {
+//     console.log('I have ' + cart[x][1] + ' ' + cart[x][0]+'s')
+// }
+
+// for(let x of cart){
+//     console.log('I have ' + x[1] + ' ' + x[0]+'s')
+// }
+
+
+
+
+//B3
+// var MyDog = {
+//     "name": "Ngáo",
+//     "legs": 4,
+//     "friends": ["everything!"]
+// };
+
+// // var Husky = {};
+// // delete Object.assign(Husky, MyDog)
+// // console.log(Husky)
+
+// MyDog.color = 'Brown';
+// delete MyDog.friends
+// console.log(MyDog)
