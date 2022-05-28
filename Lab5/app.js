5a740ad9279ce7c63bd45319b3ec90f4e960b88b
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2022-05-28 14:18:33 +0700

    commit

---
+++ Lab5/app.js
@@ -0,0 +1,19 @@
+//B1
+// var firstName = 'Khánh';
+// var surName = 'Bảo';
+// var lastName = 'Đinh';
+
+// alert(lastName + ' ' + surName + ' ' + firstName)
+// console.log(lastName + ' ' + surName + ' ' + firstName)
+
+
+//B2
+// var a = 10;
+// var b = 3;
+// var dư = a%b
+// console.log(dư)
+
+
+//B3
+// var string = "Hello my friend!!"
+// console.log(string.length)
