bf6e905fdfea5647ababd08033ba90669806181c
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2022-08-03 19:52:44 +0700

    commit

--- Project/js/signUp.js
+++ Project/js/signUp.js
@@ -2,37 +2,60 @@ const signUp = document.getElementById("sign-up");
 signUp.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const data_signup = {
-    firstName: signUp.fName.value,
-    lastName: signUp.lName.value,
-    email: signUp.email.value,
-    password: signUp.password.value,
-}
+  let username = document.getElementById("userName").value.trim();
+  let Email = document.getElementById("email").value.trim();
+  let Password = document.getElementById("password").value;
+  let ConfirmPassword = document.getElementById("confirmPassword").value;
 
-  console.log(data_signup);
+  let lowerCaseLetter = /[a-z]/g;
+  let upperCaseLetter = /[A-Z]/g;
+  let numbers = /[0-9]/g;
 
-  controller.register(data)
+  if (username.length < 6) {
+    alert("Username must be at least 6 characters");
+  } else if (Password.length < 8) {
+    alert("Password must be at least 8 characters");
+  } else if (!Password.match(lowerCaseLetter)) {
+    alert("Password must  contain a lowercase letter");
+  } else if (!Password.match(upperCaseLetter)) {
+    alert("Password must  contain a uppercase letter");
+  } else if (!Password.match(numbers)) {
+    alert("Password must  contain a number or special character");
+  } else if (Password !== ConfirmPassword) {
+    alert("Password is not confirmed correctly")
+  } else {
 
-  const register = async (data) => {
-    try {
-      await firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
-      firebase.auth().currentUser.sendEmailVerification();
-      alert("The email has been registered, please check your email");
+    const data_signup = {
+      userName: signUp.userName.value,
+      email: signUp.email.value,
+      password: signUp.password.value,
+      confirmPassword: signUp.confirmPassword.value,
+    }
+
+    console.log(data_signup);
+
+    const register = async (data) => {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
+        firebase.auth().currentUser.sendEmailVerification();
+        alert("The email has been registered, please check your email");
+      }
+      catch (err) {
+        console.log(err);
+        alert(err.message);
+      }
+    }
+
+
+
+    register(data_signup);
+
+    signUp.userName.value = ""
+    signUp.email.value = "";
+    signUp.password.value = "";
+    signUp.confirmPassword.value = "";
+
+    location.href = './login.html'
   }
-  catch (err) {
-    console.log(err);
-    alert(err.message);
-  }
-  }
-
-  register(data_signup);
-
-  signUp.fName.value = "";
-  signUp.lName.value = "";
-  signUp.email.value = "";
-  signUp.password.value = "";
-  signUp.confirmPassword.value = "";
-
-  location.href = './login.html'
 
 })
