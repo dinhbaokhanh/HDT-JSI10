cac19a329aad29aca0ae5ced4a0a555bb1515f2e
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2022-08-06 16:55:19 +0700

    commit

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,11 +1,11 @@
-// const firebaseApp = firebase.initializeApp({
-//     /* Firebase config */
-//     apiKey: "AIzaSyBsc9i_DKZeJzjdDQ9BmZdpDztbEPoLci0",
-//     authDomain: "vietnam-cuisine.firebaseapp.com",
-//     projectId: "vietnam-cuisine",
-//     storageBucket: "vietnam-cuisine.appspot.com",
-//     messagingSenderId: "122616938343",
-//     appId: "1:122616938343:web:141e7b87309954f05179c2"
-// });
-// const database = firebaseApp.firestore();
-// const auth = firebaseApp.auth();
+const firebaseApp = firebase.initializeApp({
+    /* Firebase config */
+    apiKey: "AIzaSyDsBJQpYxOr1uP__V_9-OgFF1VF7pKxyj0",
+    authDomain: "vietnam-food-cc57c.firebaseapp.com",
+    projectId: "vietnam-food-cc57c",
+    storageBucket: "vietnam-food-cc57c.appspot.com",
+    messagingSenderId: "607104677947",
+    appId: "1:607104677947:web:19a4a6cb97b7181681c41e"
+});
+const database = firebaseApp.firestore();
+const auth = firebaseApp.auth();
